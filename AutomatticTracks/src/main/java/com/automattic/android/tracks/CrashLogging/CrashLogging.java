016774b89830ad39516c5d096d973e4ea90d0038
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-03-04 12:10:29 +0100

    Enable androidx

--- AutomatticTracks/src/main/java/com/automattic/android/tracks/CrashLogging/CrashLogging.java
+++ AutomatticTracks/src/main/java/com/automattic/android/tracks/CrashLogging/CrashLogging.java
@@ -1,9 +1,10 @@
 package com.automattic.android.tracks.CrashLogging;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.automattic.android.tracks.BuildConfig;
 import com.automattic.android.tracks.TracksUser;
