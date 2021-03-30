0fdbc97865adc03c038ce165ab1f2564919754d3
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-03-30 15:48:51 +0200

    Add dependency of Tracks lib to sample app

--- AutomatticTracks/sampletracksapp/src/main/java/com/example/sampletracksapp/MainActivity.kt
+++ AutomatticTracks/sampletracksapp/src/main/java/com/example/sampletracksapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.sampletracksapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.automattic.android.tracks.TracksUser
+import com.automattic.android.tracks.crashlogging.CrashLogging
+import com.automattic.android.tracks.crashlogging.CrashLoggingDataProvider
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CrashLogging.start(
+            this,
+            object : CrashLoggingDataProvider {
+                override val sentryDSN: String
+                    get() = TODO("Not yet implemented")
+                override val userHasOptedOut: Boolean
+                    get() = TODO("Not yet implemented")
+                override val buildType: String
+                    get() = TODO("Not yet implemented")
+                override val releaseName: String
+                    get() = TODO("Not yet implemented")
+                override val currentUser: TracksUser?
+                    get() = TODO("Not yet implemented")
+                override val applicationContext: Map<String, String?>
+                    get() = TODO("Not yet implemented")
+                override val userContext: Map<String, String?>
+                    get() = TODO("Not yet implemented")
+                override val locale: Locale?
+                    get() = TODO("Not yet implemented")
+                override val enableCrashLoggingLogs: Boolean
+                    get() = TODO("Not yet implemented")
+            }
+        )
     }
 }
