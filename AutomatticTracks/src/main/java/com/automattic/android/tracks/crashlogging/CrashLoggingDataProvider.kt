72532fef3bdce6c2dc7d28532ded39b0cc71f0dc
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-03-31 11:35:19 +0200

    Update CrashLoggingDataProvider docs: remove unused parts

--- AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/CrashLoggingDataProvider.kt
+++ AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/CrashLoggingDataProvider.kt
@@ -6,36 +6,27 @@ import java.util.Locale
 interface CrashLoggingDataProvider {
     /**
      * Provides [CrashLogging] with the Sentry DSN for this application.
-     * @return The Sentry DSN for this application.
      */
     val sentryDSN: String
 
     /**
      * Provides [CrashLogging] with information on whether the user has opted out
      * of data collection.
-     *
-     * The implementation of this method should fetch this value from persistent storage, and should
-     * never call `CrashLogging.getUserHasOptedOut()`.
-     *
-     * @return A value indicating whether or not the user has opted out of data collection.
      */
     val userHasOptedOut: Boolean
 
     /**
      * Provides [CrashLogging] with information on what type of build this is.
-     * @return The build type
      */
     val buildType: String
 
     /**
      * Provides [CrashLogging] with the name of this release.
-     * @return The release name
      */
     val releaseName: String
 
     /**
      * Provides [CrashLogging] with information about the current user.
-     * @return A `TracksUser` object containing data about the current user.
      *
      * @see TracksUser
      */
