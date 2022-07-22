7587343131986c37c7441ebbc289194b9db23e50
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-07-22 13:33:07 +0200

    Suppress not crucial lint errors

--- AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/internal/SentryEventExt.kt
+++ AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/internal/SentryEventExt.kt
@@ -1,7 +1,9 @@
 package com.automattic.android.tracks.crashlogging.internal
 
+import android.annotation.SuppressLint
 import io.sentry.SentryEvent
 
+@SuppressLint("NewApi")
 internal fun SentryEvent.appendTags(tags: Map<String, String>) {
     tags.forEach { (key, value) ->
         this.setTag(key, value)
