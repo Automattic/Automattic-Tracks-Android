b76fe4aa006d48e06fcf3e28a9927a7c70a835b0
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-07-28 13:58:56 +0200

    Add description for RequestFormatter

--- AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/RequestFormatter.kt
+++ AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/RequestFormatter.kt
@@ -4,7 +4,11 @@ import okhttp3.Request
 
 /**
  * Formats OkHttp requests before using them as a breadcrumb or performance span
+ *
+ * @return formatted url, sent to Sentry
  */
 interface RequestFormatter {
-    fun formatRequestUrl(request: Request): String
+    fun formatRequestUrl(request: Request): FormattedUrl
 }
+
+typealias FormattedUrl = String
