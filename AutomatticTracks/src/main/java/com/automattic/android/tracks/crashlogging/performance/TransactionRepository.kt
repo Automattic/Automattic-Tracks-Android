2a166d3ecd5cff7602f58b4eafca30d470e8aea1
Wojtek Zieba <wojtek.zieba@automattic.com>
2022-07-29 10:07:28 +0200

    POC for TransactionRepository

---
+++ AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/performance/TransactionRepository.kt
@@ -0,0 +1,30 @@
+package com.automattic.android.tracks.crashlogging.performance
+
+import io.sentry.ITransaction
+import io.sentry.Sentry
+import java.util.UUID
+
+class TransactionRepository {
+
+    private val transactions: MutableMap<TransactionId, ITransaction> = mutableMapOf()
+
+    fun startTransaction(name: String, operation: TransactionOperation): TransactionId {
+        val transaction = Sentry.startTransaction(name, operation.value, true)
+        val uuid = TransactionId(UUID.randomUUID().toString())
+
+        transactions[uuid] = transaction
+
+        return uuid
+    }
+
+    fun finishTransaction(transactionId: TransactionId) {
+        transactions[transactionId]?.finish()
+    }
+}
+
+@JvmInline
+value class TransactionId(val value: String)
+
+enum class TransactionOperation(val value: String) {
+    UI_LOAD("ui.load")
+}
