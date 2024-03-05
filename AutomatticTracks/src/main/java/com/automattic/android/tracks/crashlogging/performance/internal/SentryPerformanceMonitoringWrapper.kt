7e2dfcee845c921d2dd82e4cdfbfbbcceaa0f0b2
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-03-05 10:38:54 +0100

    fix: address bindToScope breaking Sentry SDK change
    Context: https://github.com/getsentry/sentry-java/releases/tag/7.0.0 (Breaking Changes section)

--- AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/performance/internal/SentryPerformanceMonitoringWrapper.kt
+++ AutomatticTracks/src/main/java/com/automattic/android/tracks/crashlogging/performance/internal/SentryPerformanceMonitoringWrapper.kt
@@ -2,10 +2,15 @@ package com.automattic.android.tracks.crashlogging.performance.internal
 
 import io.sentry.ITransaction
 import io.sentry.Sentry
+import io.sentry.TransactionOptions
 
 class SentryPerformanceMonitoringWrapper {
 
     fun startTransaction(name: String, operation: String, bindToScope: Boolean): ITransaction {
-        return Sentry.startTransaction(name, operation, bindToScope)
+        return Sentry.startTransaction(
+            name,
+            operation,
+            TransactionOptions().apply { isBindToScope = bindToScope }
+        )
     }
 }
