ac4fc80fe7458f7138037918977943b8d560cd6f
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-08-22 13:32:29 +0200

    Introduce `getVariation` method

--- experimentation/src/main/java/com/automattic/android/experimentation/domain/Assignments.kt
+++ experimentation/src/main/java/com/automattic/android/experimentation/domain/Assignments.kt
@@ -1,7 +1,12 @@
 package com.automattic.android.experimentation.domain
 
+import com.automattic.android.experimentation.domain.Variation.Control
+
+
 data class Assignments(
     val variations: Map<String, Variation>,
     val timeToLive: Int,
     val fetchedAt: Long,
-)
+) {
+    fun getVariation(experiment: String) = variations[experiment] ?: Control
+}
