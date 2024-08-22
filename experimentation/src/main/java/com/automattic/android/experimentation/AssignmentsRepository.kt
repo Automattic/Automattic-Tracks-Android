f897d9e519167f2c00f40a1f2d20eda9ff9c45b7
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-08-22 11:47:40 +0200

    Introduce `AssignmentsRepository`

---
+++ experimentation/src/main/java/com/automattic/android/experimentation/AssignmentsRepository.kt
@@ -0,0 +1,39 @@
+package com.automattic.android.experimentation
+
+import com.automattic.android.experimentation.domain.Assignments
+import com.automattic.android.experimentation.local.FileBasedCache
+import com.automattic.android.experimentation.remote.ExperimentRestClient
+import org.wordpress.android.fluxc.store.ExperimentStore.Platform
+
+internal class AssignmentsRepository(
+    private val experimentRestClient: ExperimentRestClient,
+    private val cache: FileBasedCache,
+) {
+
+    suspend fun fetchAssignments(
+        platform: String,
+        experimentNames: List<String>,
+        anonymousId: String? = null
+    ): Result<Unit> {
+        val fetchResult =
+            experimentRestClient.fetchAssignments(platform, experimentNames, anonymousId)
+
+        return fetchResult.fold(
+            onFailure = {
+                Result.failure(it)
+            },
+            onSuccess = { assignments ->
+                cache.saveAssignments(assignments)
+                Result.success(Unit)
+            }
+        )
+    }
+
+    suspend fun getCachedAssignments(): Assignments? {
+        return cache.getAssignments()
+    }
+
+    suspend fun clearCachedAssignments() {
+        cache.clear()
+    }
+}
