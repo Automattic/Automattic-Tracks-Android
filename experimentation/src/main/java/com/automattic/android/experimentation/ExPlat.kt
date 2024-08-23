c5412ec1c1c9652e41b8689b57eefc2541f125a8
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-08-23 16:06:46 +0200

    Revert "Remove `activeVariations` field"
    This reverts commit 41c24f2f2ebd4a5fc98ac1a0f9caa99e6335cc90.

--- experimentation/src/main/java/com/automattic/android/experimentation/ExPlat.kt
+++ experimentation/src/main/java/com/automattic/android/experimentation/ExPlat.kt
@@ -33,10 +33,20 @@ class ExPlat internal constructor(
     private val assignmentsRepository: AssignmentsRepository,
     private val assignmentsValidator: AssignmentsValidator
 ) {
+    private val activeVariations = mutableMapOf<String, Variation>()
     private val experimentIdentifiers: List<String> = experiments.map { it.identifier }
 
     /**
      * This returns the current active [Variation] for the provided [Experiment].
+     *
+     * If no active [Variation] is found, we can assume this is the first time this method is being
+     * called for the provided [Experiment] during the current session. In this case, the [Variation]
+     * is returned from the cached [Assignments] and then set as active. If the cached [Assignments]
+     * is stale and [shouldRefreshIfStale] is `true`, then new [Assignments] are fetched and their
+     * variations are going to be returned by this method on the next session.
+     *
+     * If the provided [Experiment] was not included in [experiments], then [Control] is returned.
+     * If [isDebug] is `true`, an [IllegalArgumentException] is thrown instead.
      */
     fun getVariation(
         experiment: Experiment,
@@ -52,8 +62,10 @@ class ExPlat internal constructor(
             }
 
             emit(
-                getAssignments(if (shouldRefreshIfStale) IF_STALE else NEVER)
-                    .getVariation(experimentIdentifier)
+                activeVariations.getOrPut(experimentIdentifier) {
+                    getAssignments(if (shouldRefreshIfStale) IF_STALE else NEVER)
+                        .getVariation(experimentIdentifier)
+                }
             )
 
         }
@@ -69,6 +81,7 @@ class ExPlat internal constructor(
 
     suspend fun clear() {
         appLogWrapper.d(T.API, "ExPlat: clearing cached assignments and active variations")
+        activeVariations.clear()
         runBlocking {
             assignmentsRepository.clearCachedAssignments()
         }
