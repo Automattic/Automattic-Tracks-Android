35a1e4e773c1887c57504268e593275766d820fd
Wojtek Zieba <wojtek.zieba@automattic.com>
2025-08-05 13:42:22 +0200

    Update dependencies (AGP, Gradle)

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     compileSdk = 34
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
